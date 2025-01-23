23b505a58a5cde6277e900a46420bc82a9b8e88e
priemum <50713962+priemum@users.noreply.github.com>
2025-01-23 13:05:00 +0300

    Update Lesson-score.js

--- models/Lesson-score.js
+++ models/Lesson-score.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const lessonScoreSchema = new Schema({
-    
+      
 });
 
 module.exports = mongoose.model("LessonScore", lessonScoreSchema);
